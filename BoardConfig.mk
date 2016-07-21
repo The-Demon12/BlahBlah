f8535285362286a0d44819c3a4a533ff42a6b9a3
The-Demon12 <kartikbhalla12@gmail.com>
2016-07-21 08:50:12 +0000

    Revert "lettuce: Add vibrator cmhw interface"
    This reverts commit a2b7a8186ff69851c2a3ed2e7ec052fd38d44e7d.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,5 @@ include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 
 include device/yu/lettuce/board/*.mk
 
-# CMHW
-BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/cmhw
-
+# inherit from proprietary files
 -include vendor/yu/lettuce/BoardConfigVendor.mk
